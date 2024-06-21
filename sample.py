3d4d30b752cd586a470a9dfa2ceb18c0b5a2df8a
ittirarichan <abhishekbinish86@gmail.com>
2024-06-21 01:08:04 -0700

    june21

---
+++ sample.py
@@ -0,0 +1,38 @@
+# print("welcome")
+# print('hello')
+
+# name="Abhishek"
+# age="21"
+# weight="65"
+# print("Name:",name)
+# print("Age:",age)
+# print("Weight:",weight)
+
+
+# a=20+1j
+# print(type(a))
+
+# a=20
+# print(type(a))
+
+# a=20.6
+# print(type(a))
+
+# a="Abhi"
+# print(type(a))
+
+# a=True
+# print(type(a))
+
+# a=(20,30,40,)
+# print(type(a))
+
+# a=[20,30,40]
+# print(type(a))
+
+# a={20,20,39}
+# print(type(a))
+
+# a={"name":"Abhi","age":21}
+# print(type(a))
+
