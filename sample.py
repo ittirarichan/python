fefa5c4b7fee2edad5ea19b3da8ba53c9852c00b
ittirarichan <abhishekbinish86@gmail.com>
2024-06-22 11:26:01 +0530

    Saturday

--- sample.py
+++ sample.py
@@ -36,3 +36,183 @@
 # a={"name":"Abhi","age":21}
 # print(type(a))
 
+
+
+
+# 22/06/24
+
+# name=input("Enter name")
+# age=int(input("Enter age"))
+# weight=float(input("Enter weight"))
+# print("Name",name,"\n","Age",age,"\n","Weight",weight)
+
+# a=10
+# b=5
+# print(a+b)
+
+# a=10
+# b=5
+# print(a-b)
+
+# a=10
+# b=5
+# print(a*b)
+# 
+# a=10
+# b=5
+# print(a/b)
+
+# a=10
+# b=5
+# print(a%b)
+
+# a='Abhi'
+# b=5
+# print(a*b)
+
+
+# a=10
+# b=5
+# print(a//b)
+
+
+# a=int(input("Enter a integer number"))
+# b=int(input("Enter another integer number"))
+# c=a+b
+# print(c)
+
+
+# a=10
+# a+=10
+# print(a)
+
+
+# a=10
+# a-=20
+# print(a)
+
+# a=10
+# a*=10
+# print(a)
+
+# a=20
+# a%=5
+# print(a)
+
+# a=20
+# a/=5
+# print(a)
+
+
+# a=10
+# a//=5
+# print(a)
+
+
+# a=10
+# a**=3
+# print(a)
+
+# a=10
+# b=10
+# print(a==b)
+
+# a=10
+# b=2
+# print(a==b)
+
+# a=10
+# b=9
+# print(a!=b)
+
+# a=10
+# b=10
+# print(a!=b)
+
+
+# a=10
+# b=11
+# print(a<b)
+
+# a=12
+# b=11
+# print(a<b)
+
+
+# a=12
+# b=11
+# print(a>b)
+
+# a=12
+# b=13
+# print(a>b)
+
+# a=10
+# b=10
+# print(a<=b)
+
+# a=12
+# b=11
+# print(a<=b)
+
+# a=12
+# b=11
+# print(a>=b)
+
+# a=12
+# b=14
+# print(a>=b)
+
+# a=10
+# b=10
+# print(not(a==a or a>=b))
+
+# a=10
+# # b=15
+# print(a==b and a<=b)
+
+
+# a=10
+# b=15
+# print(a!=b or a>b)
+
+# a="Abhishek"
+# print('A' in a)
+
+# a="Abhishek"
+# print('J' in a)
+
+# a="Abhishek"
+# print('A' not in a)
+
+# a="Abhishek"
+# print('z' in a)
+
+# a=10
+# b=10
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# l=[1,2,3,4]
+# l1=[1,2,3,4]
+# print(id(l))
+# print(id(l1))
+# print(l is l1)
+
+
+# a=float(input("Enter a number"))
+# b=float(input("Enter another number"))
+
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+
+# a=float(input("Enter a number"))
+# b=float(input("Enter another number"))
+
+# if a!=b:
+#     print(True)
+# else:
+#     print("False")
