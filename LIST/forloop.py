090bed0011257164c0f1b8c17f0c83cd928e4a85
Software-9am <abhishekbinish86@gmail.com>
2024-06-28 10:48:13 +0530

    friday

---
+++ LIST/forloop.py
@@ -0,0 +1,188 @@
+# names=["manu","Akhil","Abhi",20,30]
+
+# print(names)
+
+
+# print ("manu" in names)
+       
+
+# print (names[3])
+
+
+# if "manu"in names:
+    # print("Yes")
+# else:
+    # print("No")
+
+
+# for i in names:
+    # print(i)
+
+
+
+
+# a="welcome"
+# b=""
+# for i in a:
+#     b=i+b
+# print(b)
+
+
+
+
+
+# * * *
+# * * *
+# * * *
+# * * *
+# for i in range(4):
+#     for j in range(3):
+#         print('*\n',end="")
+#     print()
+
+
+
+
+
+# 0 1 2
+# 0 1 2
+# 0 1 2
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="\t")
+#     print()
+
+
+
+
+
+# 0 0 0
+# 1 1 1
+# 2 2 2
+# for i in range(3):
+    # for j in range(3):
+        # print(i,end="\t")
+    # print()
+
+
+
+
+
+# 0 1 2
+# 3 4 5
+# 6 7 8
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="\t")
+#         a+=1
+#     print()
+
+
+
+
+
+# 0 
+# 1 1
+# 2 2 2
+# for i in range(3):
+#     for j in range(i+1):
+#         print(i,end="\t")
+#     print()
+
+
+
+
+
+# 0
+# 0 1
+# 0 1 2
+# for i in range(3):
+#     for j in range(i+1):
+#         print(j,end="\t")
+#     print()
+
+
+
+
+# 0 
+# 1 2
+# 3 4 5
+# a=0
+# for i in range(3):
+    # for j in range(i+1):
+        # print(a,end="\t")
+        # a+=1
+    # print()
+
+
+
+
+# 0
+# 1 0
+# 2 1 0
+# for i in range(3):
+#     for j in range(i+1):
+#         print(i-j,end="\t")
+#     print()
+
+
+
+
+
+# A A A 
+# B B B
+# C C C
+# a=65
+# for i in range(3):
+#     for j in range(3):
+#         print(chr(a),end='\t')
+#     print()
+#     a+=1
+
+
+# A B C 
+# D E F 
+# G H I
+# a=65
+# for i in range(3):
+#     for j in range(3):
+#         print(chr(a),end='\t')
+#         a+=1
+#     print()
+
+
+
+
+# A B C 
+# A B C 
+# A B C 
+a=67
+for i in range(3):
+    for j in range(3):
+        print(chr(a),end="\t")
+        a*=1
+    print()
+
+
+
+# A 
+# A B
+# A B C 
+
+
+
+# A 
+# A B 
+# A B C 
+
+
+
+# A 
+# B B 
+# C C C 
+
+
+# A
+# B C 
+# D E F 
--- LIST/list.py
+++ LIST/list.py
@@ -1,30 +1,10 @@
-# names=["manu","Akhil","Abhi",20,30]
-
-# print(names)
-
-
-# print ("manu" in names)
-       
-
-# print (names[3])
-
-
-# if "manu"in names:
-    # print("Yes")
-# else:
-    # print("No")
-
-
-# for i in names:
-    # print(i)
-
-
 # a=input("Enter what do you need  ")
 # l=["Abhi","Alan","Akhil","Amal","Alan",10,20,30,40,50,60]
 # if a in l:
     # print(a,"in list")
 # else:
     # print(a,"not in the list")
+
 
 
 
@@ -37,16 +17,6 @@
 
 
 
-# not correct
-# a=input("Enter the element")
-# b=input("Append/Extend/Insert")
-# l=["Abhi","Anandhu","Nikhil",20,30]
-# if b==Append:
-    # Append=l.append(a)
-    # print(append)
-# elif b==Extend:
-    # Extend=l.extend(a)
-
 
 
 # l=[1,2,3,4,5,6,7,["a","b","c"],45,66,77,88,99]
@@ -57,8 +27,10 @@
 # print(l)
 
 
-# l=[21,25.35]
 
+
+
+# l=[21,25.35]
 # print(l.index(1))
 # print(l.count(2))
 # print(l.reverse)
@@ -67,40 +39,18 @@
 # print(l)
 
 
+
+
+
 # a=int(input("Enter how many students"))
 # name=[]
 # for i in range(a):
-    # n=input("Enter Students Name")
-    # name.append(n)
+#     n=input("Enter Students Name")
+#     name.append(n)
 # print(name)
 
 
-# a="welcome"
-# b=""
-# for i in a:
-#     b=i+b
-# print(b)
 
-# * * *
-# * * *
-# * * *
-# * * *
-
-# for i in range(4):
-#     for j in range(3):
-#         print('*\n',end="")
-#     print()
-
-
-
-
-# 0 1 2
-# 0 1 2
-# 0 1 2
-# for i in range(3):
-    # for j in range(3):
-        # print(i,end="\t")
-    # print()
 
 
 # add a new list from existing list by without reduntent
@@ -110,6 +60,8 @@
     # if i not in a: 
         # a.append(i)
 # print(a)
+
+
 
 
 
@@ -123,6 +75,13 @@
 
 
 
-# sum of the list
-l=[1,2,3,'abc',20.5]
-print(type(l))
+
+# sum of the list without characters
+# l=[1,2,3,'abc',20.5]
+# n=0
+# for i in l:
+    # if type(i)==int:
+        # n+=i
+    # elif type(i)==float:
+        # n+=i
+# print(n)
