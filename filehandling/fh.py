9c090254d4431b44b7018f2875ca8efecb070c6f
ittirarichan <abhishekbinish86@gmail.com>
2024-07-10 11:18:11 +0530

    wednesday

--- filehandling/fh.py
+++ filehandling/fh.py
@@ -11,3 +11,20 @@
 # for i in range(n):
 #     a=input("Enter the name")
 #     f.write(a)
+
+
+# a=int(input("Enter a number"))
+# f=open('python/filehandling/multiplicationtable.txt','w')
+# for i in range(1,11):
+#     f.write(f"{i} * {a} = {a*i}\n")
+
+
+a = int(input("Enter a number: "))
+filename = 'python/filehandling/multiplicationtable.txt'
+
+with open(filename, 'w') as f:
+    for i in range(1, a + 1):
+        f.write(f"Multiplication Table for {i}:\n")
+        for j in range(1, 11):
+            f.write(f"{j} * {i} = {i * j}\n")
+            f.write("\n")
