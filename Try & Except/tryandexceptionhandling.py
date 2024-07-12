643e8e1f7da4b3a7f41a085cf1d45eb22a2ad70e
ittirarichan <abhishekbinish86@gmail.com>
2024-07-12 11:29:46 +0530

    Friday

---
+++ Try & Except/tryandexceptionhandling.py
@@ -0,0 +1,43 @@
+# try:
+#     a=int(input("Enter a number"))
+#     print(a)
+# except:
+#     print("Input a number")
+
+
+
+# while True:
+#     try:
+#         a=int(input("Enter a number"))
+#         print(a)
+#         break
+#     except:
+#         print("Input a number")
+
+
+
+
+# sum of the list without characters & typecasting
+
+
+# l=[1,2,3,'abc',20.5]
+# n=0
+# for i in l:
+    # try:
+        # n+=i
+    # except:
+        # pass
+# print(n)
+
+
+
+# try:
+    # print('10'+'abc')
+# except NameError:
+    # print('its name error')
+# except TypeError:
+    # print('its type error')
+# else:
+    # print('else')
+# finally:
+    # print("Finally")
---
+++ oops/class.py
@@ -0,0 +1,43 @@
+# class synnefo:
+#     def python():
+#         print('python in syn')
+#     def php():
+#         print('php')
+
+# akhil=synnefo
+# akhil.python()
+# akhil.php()
+
+
+
+
+# class synnefo:
+#     def python(s):
+#         s.database="mysql"
+#         print('python in syn')
+#     def php(self):
+#         print('php',self.database)
+
+# akhil=synnefo()
+# manu=synnefo()
+# akhil.python()
+# akhil.php()
+# print(akhil.database)
+
+
+
+class synnefo:
+    def __init__(s):
+        print('register')
+    def python(s):
+        s.database="mysql"
+        print('python in syn')
+    def php(self):
+        print('php',self.database)
+
+akhil=synnefo()
+# manu=synnefo()
+# akhil.python()
+# akhil.php()
+# print(akhil.database)
+
